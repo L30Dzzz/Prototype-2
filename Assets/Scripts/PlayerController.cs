9c40b5f72565b55e761b072f1254a1d9cb73588e
L30Dzzz <113184803+L30Dzzz@users.noreply.github.com>
2024-03-27 13:04:57 -0400

    fixed vertical and boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
     public float yRange = 10;
+    public float zRange = 16;
     public GameObject projectilePrefab; 
 
     // Start is called before the first frame update
@@ -25,10 +26,20 @@ public class PlayerController : MonoBehaviour
         {
            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-        if (transform.position.x < -xRange)
+        if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z); 
         }
+
+        if (transform.position.z < -zRange)
+        {
+           transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange); 
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
@@ -41,7 +52,7 @@ public class PlayerController : MonoBehaviour
 
         //vertical movement
         verticalInput = Input.GetAxis("Vertical");
-        transform.Translate(Vector3.down * verticalInput * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
     
     }
 
